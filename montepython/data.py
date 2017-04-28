0dbf1398339449332cbf55997c488765d324db0c
Thejs Brinckmann <brinckmann@physik.rwth-aachen.de>
2017-04-28 14:43:03 +0200

    For NH/IH: updated values from neutrino osc. exp. to Gonzalez-Garcia et al. 2014 (update 2016)

--- montepython/data.py
+++ montepython/data.py
@@ -799,8 +799,9 @@ class Data(object):
                     raise ValueError(
                         "N_ncdm is not equal to 3."
                         " This value should be exactly 3.")
-                delta_m_squared_atm=2.45e-3
-                delta_m_squared_sol=7.50e-5
+                # From Gonzalez-Garcia et al. 2014. Updated 2016: https://arxiv.org/abs/1409.5439v2
+                delta_m_squared_atm=2.484e-3 #2.45e-3
+                delta_m_squared_sol=7.49e-5 #7.50e-5
                 m1_func = lambda m1, M_tot, d_m_sq_atm, d_m_sq_sol: M_tot**2. + 0.5*d_m_sq_sol - d_m_sq_atm + m1**2. - 2.*M_tot*m1 - 2.*M_tot*(d_m_sq_sol+m1**2.)**0.5 + 2.*m1*(d_m_sq_sol+m1**2.)**0.5
                 m1,opt_output,success,output_message = fsolve(m1_func,self.cosmo_arguments['M_tot_NH']/3.,(self.cosmo_arguments['M_tot_NH'],delta_m_squared_atm,delta_m_squared_sol),full_output=True)
                 if not success == 1:
@@ -824,8 +825,9 @@ class Data(object):
                     raise ValueError(
                         "N_ncdm is not equal to 3."
                         " This value should be exactly 3.")
-                delta_m_squared_atm=-2.45e-3
-                delta_m_squared_sol=7.50e-5
+                # From Gonzalez-Garcia et al. 2014. Updated 2016: https://arxiv.org/abs/1409.5439v2
+                delta_m_squared_atm=-2.467e-3 #-2.45e-3
+                delta_m_squared_sol=7.49e-5 #7.50e-5
                 m1_func = lambda m1, M_tot, d_m_sq_atm, d_m_sq_sol: M_tot**2. + 0.5*d_m_sq_sol - d_m_sq_atm + m1**2. - 2.*M_tot*m1 - 2.*M_tot*(d_m_sq_sol+m1**2.)**0.5 + 2.*m1*(d_m_sq_sol+m1**2.)**0.5
                 m1,opt_output,success,output_message = fsolve(m1_func,self.cosmo_arguments['M_tot_IH']/3.,(self.cosmo_arguments['M_tot_IH'],delta_m_squared_atm,delta_m_squared_sol),full_output=True)
                 if not success == 1:
